a6468b3e2a13f3c2e108707163c5b445bf84472e
Sahan Wick <sahanwick5@gmail.com>
2021-12-13 21:09:49 -0600

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+// const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+window.addEventListener("load", function() {
+    
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    console.log(listedPlanetsResponse);
+    listedPlanetsResponse.then(function(result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function() {
+        console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        const planet = pickPlanet(listedPlanets);
+        console.log(planet);
+        addDestinationInfo(document,planet.name,planet.diameter,planet.star,planet.distance,planet.moons,planet.image);
+    })
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,39 +1,66 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    //const getElement = document.getElementById
+    document.getElementById("missionTarget").innerHTML = `
+    <h2>Mission Destination</h2>
+    <ol>
+        <li>Name: ${name}</li>
+        <li>Diameter: ${diameter}</li>
+        <li>Star: ${star}</li>
+        <li>Distance from Earth: ${distance}</li>
+        <li>Number of Moons: ${moons}</li>
+    </ol>
+    <img src="${imageUrl}">
+    `;
 }
 
 function validateInput(testInput) {
-   
+    if (testInput == null) { return "Empty"; }
+    if (isNaN(testInput)) { return "Not a number"; }
+    else { return "Is a number"}
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    const getElement = document.getElementById
+    const StatusList = ["pilotStatus", "copilotStatus", "fuelStatus", "cargoStatus"];
+    const launchStatus = getElement("launchStatus");
+
+    getElement(StatusList[0]).innerHTML = `Pilot ${pilot} is ready for launch`;
+    getElement(StatusList[1]).innerHTML = `Co-pilot ${copilot} is ready for launch`;
+
+    function unReady(element, text) {
+        element.innerHTML = text;
+        launchStatus.innerHTML = "Shuttle not ready for launch";
+        launchStatus.styles.color = "red";
+        list.styles.visibility = "visible";
+    }
+    if (fuelLevel < 10000) { unReady(getElement(StatusList[2]),"Fuel level too low for launch"); }
+    if (cargoLevel > 10000) { unReady(getElement(StatusList[3]),"Cargo mass too high for launch"); }
 }
 
 async function myFetch() {
-    let planetsReturned;
+    // let planetsReturned;
+    // planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+    //     response.json().then(function(json) {
+    //         return json;
+    //     });
+    // });
+    //return planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
+    return fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+        return response.json().then(function(json) {
+            return json;
         });
-
-    return planetsReturned;
+    });
 }
 
 function pickPlanet(planets) {
+    const index = Math.round((Math.random() * 100) * ((planets.length-1) / 100))
+    return planets[index];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
